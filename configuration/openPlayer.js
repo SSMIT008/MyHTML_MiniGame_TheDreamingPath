612aae53e5ddc58b6220b74d06ced16b0001e977
Stan Smit @AchillesGames <studio@achillesgames.com>
2025-08-23 14:47:37 -0400

    Refactoring

---
+++ configuration/openPlayer.js
@@ -0,0 +1,10 @@
+// Function that opens audio player
+function openPlayer() {
+    if (!window.playerWindow || window.playerWindow.closed) {
+        window.playerWindow = window.open(
+            "../music/player.html",    // This is a separate file containing your audio
+            "MusicPlayer",
+            "width=300,height=100"
+        );
+    }
+}
