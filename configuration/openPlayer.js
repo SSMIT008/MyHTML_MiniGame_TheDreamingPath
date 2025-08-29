20b144939e2ec09fde46a27f2b20171f7ab6e6db
Stan Smit @AchillesGames <studio@achillesgames.com>
2025-08-28 20:21:45 -0400

    player link updated

--- configuration/openPlayer.js
+++ configuration/openPlayer.js
@@ -2,7 +2,7 @@
 function openPlayer() {
     if (!window.playerWindow || window.playerWindow.closed) {
         window.playerWindow = window.open(
-            "../music/player.html",    // This is a separate file containing your audio
+            "./music/player.html",    // This is a separate file containing your audio
             "MusicPlayer",
             "width=300,height=100"
         );
